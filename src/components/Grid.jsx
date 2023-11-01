fb38d84248535ebcad0f30ff911220d1c687d41a
Petri <peewter@gmail.com>
2023-11-01 10:05:25 +0200

    Integrate Row component into Grid + add binary array and slicing functions

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,16 +1,45 @@
-import Row from './Row';
+import { useState } from 'react';
+import Pixel from './Pixel';
 import '../App.css';
 
 const Grid = () => {
-  let rows = [];
+  const [binaryArray, setBinaryArray] = useState(new Array(42).fill(0));
 
-  for (let i = 0; i < 7; i++) {
-    rows.push(<Row key={i} />);
-  }
+  const toggleBinaryValue = (index) => {
+    const updatedBinaryArray = [...binaryArray];
+    updatedBinaryArray[index] = 1 - updatedBinaryArray[index]; // Toggle between 0 and 1
+    setBinaryArray(updatedBinaryArray);
+  };
+
+  const arrayChunk = (arr, n) => {
+    const array = arr.slice();
+    const chunks = [];
+    while (array.length) chunks.push(array.splice(0, n));
+    return chunks;
+  };
+
+  const binaryChunks = arrayChunk(binaryArray, 21);
   return (
-    <div id='grid'>
-      <div id='pixels'>{rows}</div>
-    </div>
+    <>
+      <div className='grid'>
+        {arrayChunk(binaryArray, 6).map((row, rowIndex) => (
+          <div key={rowIndex} className='row'>
+            {row.map((binary, colIndex) => {
+              const totalIndex = rowIndex * 6 + colIndex;
+              return (
+                <Pixel
+                  key={totalIndex}
+                  binary={binary}
+                  toggleBinaryValue={() => toggleBinaryValue(totalIndex)}
+                />
+              );
+            })}
+          </div>
+        ))}
+      </div>
+      <div>{parseInt(binaryChunks[0].join(''), 2)}</div>
+      <div>{parseInt(binaryChunks[1].join(''), 2)}</div>
+    </>
   );
 };
 
