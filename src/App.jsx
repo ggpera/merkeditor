721ed0a3a51f01e465b6065cb3869af6049c8e10
Petri <peewter@gmail.com>
2023-11-09 12:05:29 +0200

    Implement function for setting the character to be edited + add CharacterSet component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import Grid from './components/Grid';
 import './App.css';
+import { useState } from 'react';
+import Grid from './components/Grid';
+import CharacterSet from './components/CharacterSet';
+import characters from './characters.json';
 
 const App = () => {
+  const [character, setCharacter] = useState(characters.set[0].binaryArray);
+
+  const setEditCharacter = (char) => {
+    setCharacter(char.binaryArray);
+  };
+  console.log(character);
   return (
     <div className='App'>
-      <Grid />
+      <div className='character-set'>
+        <CharacterSet characters={characters.set} setEditCharacter={setEditCharacter} />
+      </div>
+      <Grid key={character} character={character} />
     </div>
   );
 };
