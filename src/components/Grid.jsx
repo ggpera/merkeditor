f861b50e10af20d0399b662bd4486d47fac5ba0c
Petri <peewter@gmail.com>
2023-11-10 19:24:55 +0200

    Pass character prop to Grid component

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,24 +1,26 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 import { arrayChunk } from '../utils/arrays';
 import Pixel from './Pixel';
-import '../App.css';
-import characters from '../characters.json';
 
-const Grid = () => {
-  const [binaryArray, setBinaryArray] = useState(characters.set[0].binaryArray);
+const Grid = ({ character }) => {
+  const [binaryArray, setBinaryArray] = useState(character);
 
+  // Set the binary value of an item to be 0 or 1
   const toggleBinaryValue = (index) => {
     const updatedBinaryArray = [...binaryArray];
     updatedBinaryArray[index] = 1 - updatedBinaryArray[index]; // Toggle between 0 and 1
     setBinaryArray(updatedBinaryArray);
   };
 
+  // Zero the array with clear button
   const resetBinaryArray = () => {
     const updatedBinaryArray = [...binaryArray];
     updatedBinaryArray.fill(0);
     setBinaryArray(updatedBinaryArray);
   };
 
+  // Split the original array of 42 items into two arrays, each containing 21 items
   const binaryChunks = arrayChunk(binaryArray, 21);
 
   console.log(binaryArray);
@@ -50,5 +52,7 @@ const Grid = () => {
     </>
   );
 };
-
+Grid.propTypes = {
+  character: PropTypes.array,
+};
 export default Grid;
