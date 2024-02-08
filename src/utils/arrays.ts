c6efc10e29dfa3a8fbe90e2415e5c5f013a4140d
Petri <peewter@gmail.com>
2024-02-08 10:39:57 +0200

    Define parameter types

--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -1,4 +1,4 @@
-export const arrayChunk = (arr, n) => {
+export const arrayChunk = (arr: Array<any>, n: number) => {
   const array = arr.slice();
   const chunks = [];
   while (array.length) chunks.push(array.splice(0, n));
