78b9ba08d92b5ae84af44f4454f03e968dada365
Petri <peewter@gmail.com>
2024-02-08 10:40:29 +0200

    Define state types

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -3,32 +3,34 @@ import { useState } from 'react';
 import Grid from '../Grid/Grid';
 import CharacterSet from '../CharacterSet/CharacterSet';
 import data from '../../data/characters.json';
+import { Character, Characters } from '../../types';
 
 const Editor = () => {
-  const [characters, setCharacters] = useState(data);
-  const [character, setCharacter] = useState(characters.set[0].binaryArray);
-  const [charIndex, setCharIndex] = useState(0);
+  const [characters, setCharacters] = useState<Characters>(data);
+  const [character, setCharacter] = useState<Array<number>>(characters.set[0].binaryArray);
+  const [charIndex, setCharIndex] = useState<number>(0);
 
   // Set character from set to be edited
-  const setEditCharacter = (char, index) => {
+  const setEditCharacter = (char: Character, index: number) => {
     setCharacter(char.binaryArray);
     setCharIndex(index);
   };
   // Save updated character to character set and re-render
-  const saveCharacters = (char) => {
+  const saveCharacters = (char: Array<number>) => {
     const updatedCharacters = { ...characters };
     updatedCharacters.set[charIndex].binaryArray = char;
     setCharacters(updatedCharacters);
   };
   console.log(character);
+
   return (
     <div className={styles.editor}>
       <div className={styles.grid}>
-        <Grid key={character} character={character} saveCharacters={saveCharacters} />
+        <Grid key={charIndex} charBinary={character} saveCharacters={saveCharacters} />
       </div>
       <div className={styles.characterSet}>
         <CharacterSet
-          key={characters.set}
+          key={characters.id}
           characters={characters.set}
           setEditCharacter={setEditCharacter}
         />
