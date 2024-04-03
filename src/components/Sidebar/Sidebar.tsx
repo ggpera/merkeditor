648883040710dd6b51787843264641065d874d72
Petri <peewter@gmail.com>
2024-04-03 14:30:30 +0300

    Add sidebar

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import styles from './Sidebar.module.css';
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      <button></button>
+    </div>
+  );
+};
+
+export default Sidebar;
