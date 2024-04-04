fda71e51371a567a24412d6d70a5aa1d5aac9c2a
Petri <peewter@gmail.com>
2024-04-04 11:31:24 +0300

    Import sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './assets/global.css';
 import Editor from './components/Editor/Editor';
+import Sidebar from './components/Sidebar/Sidebar';
 
 const App = () => {
   return (
-    <>
+    <div style={{ display: 'flex' }}>
+      <Sidebar />
       <Editor />
-    </>
+    </div>
   );
 };
 
