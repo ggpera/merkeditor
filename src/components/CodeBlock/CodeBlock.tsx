2e0156769a66edec42e5eab0c549842e287e9986
Petri <peewter@gmail.com>
2024-04-16 13:12:29 +0300

    Implement select text functionality

--- src/components/CodeBlock/CodeBlock.tsx
+++ src/components/CodeBlock/CodeBlock.tsx
@@ -1,6 +1,7 @@
 import styles from './CodeBlock.module.css';
 import { Character, Characters } from '../../types';
 import { arrayChunk } from '../../utils/arrays';
+import { useRef } from 'react';
 
 type Props = {
   characterSet: Characters;
@@ -8,10 +9,27 @@ type Props = {
 
 const CodeBlock = ({ characterSet }: Props) => {
   const characters = characterSet.set;
+  const textRef = useRef<HTMLDivElement>(null);
+
+  const selectText = () => {
+    if (textRef.current) {
+      // Create a range object
+      const range = document.createRange();
+      // Select div content
+      range.selectNode(textRef.current);
+      // Ensure that the selection range is cleared
+      window.getSelection()?.removeAllRanges();
+      // Add it to the current selection
+      window.getSelection()?.addRange(range);
+    }
+  };
+
   return (
     <>
-      <button className={styles.copyBtn}>Copy</button>
-      <div className={styles.codeblock}>
+      <button className={styles.selectBtn} onClick={selectText}>
+        Select all
+      </button>
+      <div className={styles.codeblock} ref={textRef}>
         {characters.map((character: Character) => {
           const binaryChunks = arrayChunk(character.binaryArray, 21);
           const x = parseInt(binaryChunks[0].join(''), 2).toFixed(1);
